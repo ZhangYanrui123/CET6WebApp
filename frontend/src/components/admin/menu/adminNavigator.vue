9808b61fde03146dff5e42d41c90b3a613cc4317
xingyu <capriccio_xy@sina.com>
2023-07-02 09:57:02 +0800

    admin首页

--- frontend/src/components/admin/menu/adminNavigator.vue
+++ frontend/src/components/admin/menu/adminNavigator.vue
@@ -5,10 +5,10 @@
             <span v-if="isCollapse" class="iconfont icon-left-indent" @click="changeMenu"></span>
         </div>
         <el-breadcrumb separator-class="el-icon-arrow-right">
-            <el-breadcrumb-item :to="{ path: '/admin' }">首页</el-breadcrumb-item>
-            <el-breadcrumb-item :to="{ path: '/admin/adminStudent/studentList' }">学生中心</el-breadcrumb-item>
-            <el-breadcrumb-item :to="{ path: '/admin/adminTeacher/teacherList' }">教师中心</el-breadcrumb-item>
-            <el-breadcrumb-item :to="{ path: '/admin/adminExam/examList' }">考场中心</el-breadcrumb-item>
+            <el-breadcrumb-item :to="{ path: '/admin' }" class="custom-color">首页</el-breadcrumb-item>
+            <el-breadcrumb-item :to="{ path: '/admin/adminStudent/studentList' }" class="custom-color">学生中心</el-breadcrumb-item>
+            <el-breadcrumb-item :to="{ path: '/admin/adminTeacher/teacherList' }" class="custom-color">教师中心</el-breadcrumb-item>
+            <el-breadcrumb-item :to="{ path: '/admin/adminExam/examList' }" class="custom-color">考场中心</el-breadcrumb-item>
             <el-breadcrumb-item>...</el-breadcrumb-item>
         </el-breadcrumb>
     </div>
@@ -27,11 +27,17 @@ export default {
 </script>
 
 <style scoped>
+
+.custom-color {
+  color: #ccc;
+}
+
 .adminNavigator{
     //height: 50px;
     background: #1e78bf;
     color: #fff;
     line-height: 50px;
+    padding-bottom: 15px;
     .open{
         .iconfont{
             font-size: 22px;
@@ -39,4 +45,5 @@ export default {
         }
     }
 }
+
 </style>
